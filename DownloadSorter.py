849f83d20b05a80bf41a6715ed1ef5cb476d4b1e
Isaac <isaacatkinson04@gmail.com>
2025-05-30 02:43:40 +0100

    Loop through specified files and move them to specified directories

--- DownloadSorter.py
+++ DownloadSorter.py
@@ -5,10 +5,21 @@ from pathlib import Path
 # Access the download folder of system
 downloads = Path.home() / "Downloads"
 
-# loop through the download folder and search for files and exclude folders
+# loop through the download folder and only search for .pdf files and exclude folders
 # .iterdir() is needed to loop through the paths
 for i in downloads.iterdir():
 
     # i.is_file only checks for files
-    if i.is_file():
-        print(i)
+    # i.suffix only checks for the file type
+    if i.is_file() and i.suffix == '.pdf':
+
+        # shutil.move will move the file
+        # first specify the files that will move (i - the source)
+        # and then specify where they will move to (the destination)
+        shutil.move(i, Path.home() / "Downloads" / "Pdfs")
+
+# loop through the download folder and only search for .exe files and exclude folders
+for i in downloads.iterdir():
+
+    if i.is_file() and i.suffix == '.exe':
+        shutil.move(i, Path.home() / "Downloads" / "Installers")
