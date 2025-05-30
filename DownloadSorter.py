d7fb699f6d8f2193777e6009f0bf51a31bc817c3
Isaac <isaacatkinson04@gmail.com>
2025-05-30 02:28:21 +0100

    Access download folder Loop through the files Print the files

--- DownloadSorter.py
+++ DownloadSorter.py
@@ -1,3 +1,14 @@
 import os
 import shutil
+from pathlib import Path
 
+# Access the download folder of system
+downloads = Path.home() / "Downloads"
+
+# loop through the download folder and search for files and exclude folders
+# .iterdir() is needed to loop through the paths
+for i in downloads.iterdir():
+
+    # i.is_file only checks for files
+    if i.is_file():
+        print(i)
