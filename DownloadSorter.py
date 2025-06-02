e1b73daaf09bf17c44cb66f5bf628e006e6a165f
Isaac <isaacatkinson04@gmail.com>
2025-06-02 23:21:41 +0100

    Created function to create directories for files and move them

--- DownloadSorter.py
+++ DownloadSorter.py
@@ -5,26 +5,32 @@ from pathlib import Path
 # Access the download folder of system
 downloads = Path.home() / "Downloads"
 
-# Dictionary of known extensions
-knownExtensions = {".pdf" : ".pdf",
-                   ".exe" : ".exe",
-                   ".msi" : ".msi"}
+def sort_files(file):
 
-# loop through the download folder and only search for .pdf files and exclude folders
-# .iterdir() is needed to loop through the paths
-for i in downloads.iterdir():
+    # loop through the download folder and only search for .pdf files and exclude folders
+    # .iterdir() is needed to loop through the paths
+    for i in downloads.iterdir():
 
-    # i.is_file only checks for files
-    # i.suffix only checks for the file type
-    if i.is_file() and i.suffix == '.pdf':
+        # i.is_file only checks for files
+        # i.suffix only checks for the file type
+        if i.is_file():
+
+            # variable to store file extensions
+            extension = file.suffix
+
+            if extension:
+
+                # name the folder after the extension
+                folder_name = extension
+
+                # the file path for the extension
+                folder_path = downloads / folder_name
+
+                # make the new directory
+                folder_path.mkdir(exist_ok=True)
+
 
         # shutil.move will move the file
         # first specify the files that will move (i - the source)
         # and then specify where they will move to (the destination)
-        shutil.move(i, Path.home() / "Downloads" / ".pdf")
-
-# loop through the download folder and only search for .exe files and exclude folders
-for i in downloads.iterdir():
-
-    if i.is_file() and i.suffix == '.exe':
-        shutil.move(i, Path.home() / "Downloads" / ".exe")
+        shutil.move(str(i), str(Path.home() / "Downloads"))
