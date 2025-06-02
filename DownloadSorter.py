f83ed1f770f5d030b2f82efb622c138fdfdf5850
Isaac <isaacatkinson04@gmail.com>
2025-06-02 22:08:12 +0100

    Dictionary of existing extensions, Move specific file extensions

--- DownloadSorter.py
+++ DownloadSorter.py
@@ -5,6 +5,11 @@ from pathlib import Path
 # Access the download folder of system
 downloads = Path.home() / "Downloads"
 
+# Dictionary of known extensions
+knownExtensions = {".pdf" : ".pdf",
+                   ".exe" : ".exe",
+                   ".msi" : ".msi"}
+
 # loop through the download folder and only search for .pdf files and exclude folders
 # .iterdir() is needed to loop through the paths
 for i in downloads.iterdir():
@@ -16,10 +21,10 @@ for i in downloads.iterdir():
         # shutil.move will move the file
         # first specify the files that will move (i - the source)
         # and then specify where they will move to (the destination)
-        shutil.move(i, Path.home() / "Downloads" / "Pdfs")
+        shutil.move(i, Path.home() / "Downloads" / ".pdf")
 
 # loop through the download folder and only search for .exe files and exclude folders
 for i in downloads.iterdir():
 
     if i.is_file() and i.suffix == '.exe':
-        shutil.move(i, Path.home() / "Downloads" / "Installers")
+        shutil.move(i, Path.home() / "Downloads" / ".exe")
